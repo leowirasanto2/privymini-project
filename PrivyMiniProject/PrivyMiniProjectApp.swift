ffcd017efac97d352e759b9d914f0bfe28f20be9
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-08-22 12:08:37 +0800

    init project

---
+++ PrivyMiniProject/PrivyMiniProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivyMiniProjectApp.swift
+//  PrivyMiniProject
+//
+//  Created by Leo Wirasanto Laia on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PrivyMiniProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
